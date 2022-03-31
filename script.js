dfdf5771eb381d10b9f6b9d6316567aaa273b398
faridev18 <zfarihane@gmail.com>
2022-03-31 16:07:12 +0100

    hacking animation with javascript

---
+++ script.js
@@ -0,0 +1,137 @@
+$(document).ready(function () {
+  const textArray = textString.split("");
+  const textElem = $(".hacking-animation__text");
+
+  let count = 0;
+
+  setInterval(() => {
+    if (textArray[count] === "\n") {
+      textElem.append("<br>");
+    } else {
+      textElem.append(
+        `<span class="hacking-animation__character">${textArray[count]}</span>`
+      );
+    }
+
+    count++;
+    if (count === textArray.length) {
+      count = 0;
+    }
+  }, 20);
+});
+
+var textString = `.
+.
+/// F A R I D E V ///
+    <project ENDLESS>:::MEF1ST0
+           Commence take control module RS232C.10.10.20
+           ...Connecting server K.A.R.I.O.N....
+...
+...
+//infected devices\\\\
+            ://remote control...
+            (**ACCESS GRANTED**)....
+                    message for victims="I'm inside! HaHa FUCK YOU! Im FARIDEV" ....
+                    ....
+                    ...
+                    ..
+                    .
+                    //STOP CALLING Victim...
+                    
+{HACKING PROSSESS}/// -- \\\\ Klein\\\\
+  <M> Connecting Victim..:":":":
+  ff
+
+<Guardian Devil...>
+                /uploading system
+                
+                     K.A.R.I.O.N. ctOS
+                 Central Operating System 
+            highly advanced digital information 
+        system designed to manage the infrastructure 
+                         of city
+                -----I´m your future----
+                     klein404@ReSec;
+                     
+.
+.
+.
+         START;;'DECRYPTION;;'PASSWORD
+LOAD...
+...
+   ...
+...
+   ...
+   ..
+   .
+$%$%$qazWSXedcRFVtgbYHNujmIKol$%$%$%
+      \\\\...SERVER ACCEPTED your CALL...
+      \\\\%^^attach{codemonkey}"'';
+
+///
+//
+
+/
+//
+////CODEMONKEYSENGAGE...&*&&*&;
+      [sd]
+       {hard like Shirt}
+
+ZXC..<>Ckfkd<>vkvodkskclc...
+                         { DARIUS3421D;CODE10011011101011
+ ..-=          
+ (B+ HACKING..//MYCITYKLATOVY )
+<\\\\boeie\\\\..www.klatovy.cz\\\\
+**(AR)12. (696); (96).ffkeoslk
+             0617513060 <<<<>OKAY
+             {FOntsize}:Hard.. FONTMANAGE 22 //
+             ((open duck)) //MONSTER_FRAME;
+                (port Opened)
+                        (portisnotfound)// B1 }
+..
+..
+.....
+TARGET >> {ENTRACE GATE} break security
+--start decoding--
+10302394569822690343183456006393514030472161234121052669813370023279032359149210702468674
+
+ 1GATE--20243202619020     BIND SUCCESSFUL 
+ ;;' /
+ 2GATE--72520604305,
+        6187005192331      BIND SUCCESSFUL                 
+ 0011011011101110110101101
+-----SCANNING-----
+//////////////////
+\\\\\\\\\\\\\\\\\\
+//////////////////
+\\\\\\\\\\\\\\\\\\
+//////////////////
+\\\\\\\\\\\\\\\\\\
+/////INPUT FOUND
+>>>>>>>>>>>>>>>>>>>>>>>   (     |    )  
+                          /     |    \\
+                         /     / \\    \\
+                         |    FARIDEV   |
+                         |    inside    |
+/string found
+///BetterCheckYourDrawers
+....COPYING DATA TO THE SERVER
+
+>>GPS LOCATION
+>>PHONE CALLS
+>>TEXT MESSAGES
+>>EMAILS
+>>IDENTITY
+>>MEDIA
+>>OTHERS
+          ///(SHUTDOWN HACKING)
+             {WHIPEDRIVE}
+          whipe not found..
+S1293 K // P 430
+         K.A.R.I.O.N... VERSION 2.0
+...
+.....
+...
+....
+...
+HACKING SUCCESSFUL`;
